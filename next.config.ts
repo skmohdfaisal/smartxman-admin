80585cf7e33efbddc769c6947909ce7fa5035684
smf07 <skmohdfaisal07@gmail.com>
2026-05-23 00:49:12 +0530

    fix: allow external images in next.config for previews

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,18 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
